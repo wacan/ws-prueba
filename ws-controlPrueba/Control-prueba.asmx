8c8d2bcddd6ce2dff30486fb367caf1c8e1e8afc
wacan <65459249+wacan@users.noreply.github.com>
2020-07-29 15:07:43 -0500

    entrega 2

---
+++ ws-controlPrueba/Control-prueba.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Control-prueba.asmx.cs" Class="ws_controlPrueba.Control_prueba" %>
---
+++ ws-controlPrueba/Control-prueba.asmx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ws_controlPrueba
+{
+    /// <summary>
+    /// Descripción breve de Control_prueba
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Control_prueba : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        [WebMethod]
+        public bool prueba()
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
